f7fed69f31bc074f7ad40e8fc51f7c6dfe8c12f3
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-29 16:44:28 +0000

    factorial

--- do_while.py
+++ do_while.py
@@ -17,15 +17,14 @@ def main():
     # process
     try:
         user_number = int(user_string)
-        while loop_number < user_number:
-            loop_number = loop_number + 1
-            answer_number = answer_number * loop_number
-        # output
-        print(
-            "The factorial of all positive number from 1 to {0} is {1}".format(
-                user_number, answer_number
-            )
-        )
+        if user_number < 0:
+            print("You did not enter a psoitive integer.")
+        else:
+            while loop_number < user_number:
+                loop_number = loop_number + 1
+                answer_number = answer_number * loop_number
+            # output
+            print("{0}! = {1}".format(user_number, answer_number))
     except Exception:
         # output
         print("{} is not a valid input.".format(user_string))
